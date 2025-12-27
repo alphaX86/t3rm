8add235f0e38a7228cff55a6b706ff3177effe1d
Aadhitya A <aadhitya864@gmail.com>
2025-12-27 21:07:49 +0530

    Modify txt

--- components/terminal/Terminal.tsx
+++ components/terminal/Terminal.tsx
@@ -18,7 +18,7 @@ const ASCII_LOGO = [
   "    ██║   ██████╔╝██║  ██║██║ ╚═╝ ██║",
   "    ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝",
   "",
-  "v.1.0.0 - Terminal Portfolio",
+  "v.1.0.1 - Terminal Portfolio",
   "",
   "Hello @guest! Welcome to my terminal portfolio.",
   "Type 'help' to see available commands.",
